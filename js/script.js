689d84cc4e733d75545d4229f5cb4547c5eb7c10
hoanghongchuong <chuonghoanhong@gmail.com>
2019-05-03 20:10:33 +0700

    a

---
+++ js/script.js
@@ -0,0 +1,80 @@
+$(document).ready(function($){	
+	var owl = $(".owl-carousel-product");
+	  	owl.owlCarousel({
+	  		margin:0, 							
+	  		loop:true, 							
+	  		nav:true, 							
+	  		navText:['<i class="fa fa-caret-left"></i>','<i class="fa fa-caret-right"></i>'], 
+	  		autoplay:false, 						
+	  		autoplayTimeout:1500,
+			autoplayHoverPause:true,
+			autoplaySpeed: 2000,
+			responsiveClass:true, 				
+		    responsive:{
+		        0:{
+		            items:2,									            									            
+		        },
+		        600:{
+		            items:4,          
+		        },
+		        1000:{
+		            items:4,  
+		        }
+		    }
+		});
+
+		
+
+		var owl = $(".owl-carousel-slider");
+	  	owl.owlCarousel({
+	  		margin:0, 							
+	  		loop:true, 							
+	  		nav:true, 							
+	  		navText:['<i class="fa fa-caret-left"></i>','<i class="fa fa-caret-right"></i>'], 
+	  		autoplay:false, 						
+	  		autoplayTimeout:1500,
+			autoplayHoverPause:true,
+			autoplaySpeed: 2000,
+			responsiveClass:true, 				
+		    responsive:{
+		        0:{
+		            items:1,									            									            
+		        },
+		        600:{
+		            items:4,          
+		        },
+		        1000:{
+		            items:4,  
+		        }
+		    }
+		});
+
+});
+
+var MobileMenu = function () {
+        var mobileMenu = $("#menu");
+
+        if (mobileMenu.length) {
+            mobileMenu.mmenu({
+                "extensions": [
+                    // "fx-panels-zoom",
+                    "pagedim-black",
+                    // "theme-dark"
+                ],
+                "offCanvas": {
+                    // "position": "right"
+                },
+                "navbars": [
+                    {
+                        "position": "top",
+                        "content": [
+                            "<a class='fa fa-envelope' href='#/'></a>",
+                            "<a class='fa fa-twitter' href='#/'></a>",
+                            "<a class='fa fa-facebook' href='#/'></a>"
+                        ]
+                    }
+                ]
+            });
+        }
+
+};
