d6a1229fa6fa77dd3808ebe1afae97ae2d540296
hoanghongchuong <chuonghoanhong@gmail.com>
2019-05-04 19:22:11 +0700

    a

--- js/script.js
+++ js/script.js
@@ -49,6 +49,43 @@ $(document).ready(function($){
 		    }
 		});
 
+	$('.slider-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: false,
+	  fade: true,
+	  asNavFor: '.slider-nav',
+		 autoplay: true
+	});
+	$('.slider-nav').slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  asNavFor: '.slider-for',
+	  dots: false,
+	  centerMode: true,
+	  focusOnSelect: true,
+	  responsive: [{
+ 			breakpoint: 1024,
+ 			settings: {
+ 				slidesToShow:4,
+ 				slidesToScroll: 4
+ 			}
+ 		}, {
+ 			breakpoint: 640,
+ 			settings: {
+ 				slidesToShow: 3,
+ 				slidesToScroll: 3,
+			}
+ 		}, {
+ 			breakpoint: 420,
+ 			settings: {
+ 				slidesToShow: 3,
+ 				slidesToScroll: 3,
+		}
+ 		}]
+
+	});
+
 });
 
 var MobileMenu = function () {
